a9697450b0165754db6d301f9e7c88f82ccbc880
MaxWell61121 <vanatizadehasan@gmail.com>
2022-10-10 11:57:02 +0400

    Task tamamlandi

---
+++ BoyÇeki/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BoyÇeki
+{
+    partial class Frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbBoy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LbKilo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.yekun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Boy(sm)";
+            // 
+            // lbBoy
+            // 
+            this.lbBoy.Location = new System.Drawing.Point(102, 30);
+            this.lbBoy.Name = "lbBoy";
+            this.lbBoy.Size = new System.Drawing.Size(102, 22);
+            this.lbBoy.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Çəki(kq)";
+            // 
+            // LbKilo
+            // 
+            this.LbKilo.Location = new System.Drawing.Point(102, 58);
+            this.LbKilo.Name = "LbKilo";
+            this.LbKilo.Size = new System.Drawing.Size(102, 22);
+            this.LbKilo.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yaş";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(102, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // yekun
+            // 
+            this.yekun.Location = new System.Drawing.Point(102, 113);
+            this.yekun.Name = "yekun";
+            this.yekun.Size = new System.Drawing.Size(102, 42);
+            this.yekun.TabIndex = 6;
+            this.yekun.Text = "Hesbla";
+            this.yekun.UseVisualStyleBackColor = true;
+            this.yekun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 198);
+            this.Controls.Add(this.yekun);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LbKilo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbBoy);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BiyÇeki İdexi";
+            this.Load += new System.EventHandler(this.Frm1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lbBoy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LbKilo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button yekun;
+    }
+}
+
---
+++ BoyÇeki/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoyÇeki
+{
+    public partial class Frm1 : Form
+    {
+        private double yas;
+        private double IdealCeki;
+        private double ferq1;
+        private double ferq;
+        public Frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(lbBoy.Text.Length>0)
+            {
+                double boy = Convert.ToDouble(lbBoy.Text);
+                double ceki = Convert.ToDouble(LbKilo.Text);
+                hesabla(boy, ceki);
+            }
+           else
+            {
+                MessageBox.Show("daxil edin");
+            }
+
+        }
+        void hesabla(double boy ,double ceki)
+        {
+            double yekun = ceki / (boy * boy);
+
+            if(yekun >0 && yekun <18.4)
+            {
+                MessageBox.Show("Zayifsiniz" + "\n" + Kiloferqihesabla(boy, yekun,ceki));
+            }
+            else if ( yekun>18.5 && yekun <24.9)
+            {
+                MessageBox.Show(" Cekiniz Normaldir");
+            }
+            else if( yekun >25 && yekun <29.9)
+            {
+                MessageBox.Show(" Cekiniz COXDUR" + "\n"+ Kiloferqihesabla(boy, yekun, ceki));
+                Kiloferqihesabla(boy,yekun,ceki);
+            }
+            else if(yekun >30 && yekun <34.9)
+            {
+                MessageBox.Show(" 1 Cekiniz COXDUR"+ "\n"+ Kiloferqihesabla(boy, yekun, ceki));
+                Kiloferqihesabla(boy, yekun, ceki);
+                
+            }
+            else if(yekun> 35 && yekun <44.9)
+            {
+                MessageBox.Show("  2 Cekiniz COXDUR" + "\n"+ Kiloferqihesabla(boy, yekun, ceki));
+                Kiloferqihesabla(boy, yekun, ceki);
+            }
+            else
+            {
+                MessageBox.Show("  3 Cekiniz COXDUR" + "\n"+ Kiloferqihesabla(boy, yekun, ceki));
+                Kiloferqihesabla(boy, yekun, ceki);
+            }
+
+        }
+        string Kiloferqihesabla( double boy , double yekun, double kilo)
+        {
+            double yas;
+            yas = Convert.ToDouble(textBox3.Text);
+            IdealCeki = ((boy*100) - 100 + yas / 10) * 0.9;
+            ferq = kilo - IdealCeki;
+
+            ferq1 = IdealCeki - kilo;
+            if(kilo>IdealCeki)
+            {
+                return (ferq + " : kq ariqlamalisiz");
+            }
+            else
+            {
+                return (ferq + " : kq almalisiz");
+            }
+        }
+
+        private void Frm1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
